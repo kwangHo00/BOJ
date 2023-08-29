07ca1ba33977981588f9929bd084a3c31b092a78
KwangHo <kevin7517@naver.com>
2023-08-29 12:03:10 +0900

    BOJ Practice

---
+++ src/Minimum_Spanning_Tree/No14950.java
@@ -0,0 +1,73 @@
+//No14950_정복_두번만에 정답_1) t를 잘못 증가 시킴
+package Minimum_Spanning_Tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class No14950 {
+	static class NodeInfo{
+		int a, b, c;
+		NodeInfo(int a, int b, int c){
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+	}
+	
+	static int getParent(int[] parent, int n) {
+		if(parent[n] == n) return parent[n];
+		else return parent[n] = getParent(parent, parent[n]);
+	}
+	
+	static void union(int[] parent, int a, int b) {
+		int aParent = getParent(parent, a);
+		int bParent = getParent(parent, b);
+		if(aParent < bParent) parent[bParent] = aParent;
+		else parent[aParent] = bParent;
+	}
+	
+	static boolean find(int[] parent, int a, int b) {
+		int aParent = getParent(parent, a);
+		int bParent = getParent(parent, b);
+		if(aParent == bParent) return true;
+		else return false;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		int[] parentNode = new int[n + 1];
+		ArrayList<NodeInfo> list = new ArrayList<NodeInfo>();
+		
+		for(int i = 1; i <= n; i++) {
+			parentNode[i] = i;
+		}
+		
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			list.add(new NodeInfo(a, b, c));
+		}
+		
+		Collections.sort(list, (a, b) -> a.c - b.c);
+		
+		long minCost = 0, cnt = 0;
+		for(int i = 0; i < m; i++) {
+			if(!find(parentNode, list.get(i).a, list.get(i).b)) {
+				union(parentNode, list.get(i).a, list.get(i).b);
+				minCost += list.get(i).c + t * cnt++;
+			}
+		}
+		System.out.println(minCost);
+	}
+}
