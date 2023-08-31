999761824ab10606b3f155853d6e2e1f3e7e3659
KwangHo <kevin7517@naver.com>
2023-08-31 21:54:05 +0900

    BOJ Practice

---
+++ src/Data_Structures/No1269.java
@@ -0,0 +1,39 @@
+//No1269_대칭 차집합_정답
+package Data_Structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class No1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashSet<Integer> setA = new HashSet<>();
+        HashSet<Integer> setB = new HashSet<>();
+        HashSet<Integer> tmp = new HashSet<>();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < a; i++){
+            int value = Integer.parseInt(st.nextToken());
+            setA.add(value);
+            tmp.add(value);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < b; i++){
+            int value = Integer.parseInt(st.nextToken());
+            setB.add(value);
+        }
+
+        setA.removeAll(setB);
+        setB.removeAll(tmp);
+        System.out.println(setA.size() + setB.size());
+    }
+}
