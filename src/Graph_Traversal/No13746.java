13750d125bc1957cd569e7d932e2a2736df26706
KwangHo <kevin7517@naver.com>
2023-10-05 14:49:59 +0900

    BOJ Practice

---
+++ src/Graph_Traversal/No13746.java
@@ -0,0 +1,71 @@
+//No13746_Islands_정답
+package Graph_Traversal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class No13746 {
+    static final int[] upDown = {-1, 1, 0, 0};
+    static final int[] leftRight = {0, 0, -1, 1};
+    static char[][] map;
+    static boolean[][] isVisited;
+    static Queue<Pair> queue = new LinkedList<>();
+
+    static class Pair{
+        int x, y;
+        Pair(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static void bfs(int x, int y, int sizeN, int sizeM){
+        isVisited[x][y] = true;
+        queue.add(new Pair(x, y));
+
+        while(!queue.isEmpty()){
+            int currX = queue.peek().x;
+            int currY = queue.peek().y;
+            queue.poll();
+
+            for(int i = 0; i < 4; i++){
+                int nextX = currX + upDown[i];
+                int nextY = currY + leftRight[i];
+                if(nextX < 0 || nextX > sizeN - 1 || nextY < 0 || nextY > sizeM - 1) continue;
+                if((map[nextX][nextY] == 'L' || map[nextX][nextY] == 'C') && !isVisited[nextX][nextY]){
+                    isVisited[nextX][nextY] = true;
+                    queue.add(new Pair(nextX, nextY));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+        map = new char[n][m];
+        isVisited = new boolean[n][m];
+
+        for(int i = 0; i < n; i++) map[i] = br.readLine().toCharArray();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(map[i][j] == 'L' && !isVisited[i][j]){
+                    bfs(i, j, n, m);
+                    answer += 1;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
